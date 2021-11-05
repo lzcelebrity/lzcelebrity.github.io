4516c3be8fbd429a0cba3f048d9a24973a421774
lzcelebrity <93430938+lzcelebrity@users.noreply.github.com>
2021-11-05 18:49:20 +0200

    Add files via upload

---
+++ src/loadPage.js
@@ -0,0 +1,17 @@
+function loadPage(){
+
+
+	// HEADER
+	let elem_header = document.getElementById("header");
+
+	//elem_header.innerHTML = '';
+
+	// FOOTER
+	let elem_footer = document.getElementById("footer");
+/*
+	elem_header.innerHTML = '<div class="information"><br>LZ Celebrity<br><br><p> Designer L. Zaitseva </p><p> Ukrainian fashion brand </p><p> <a href="index.html">lzcelebrity@gmail.com</a></p><p> Kyiv, Ukraine</p><div class="icon"><a href="https://www.instagram.com"><img src="img/instagram_w.png"></a></div><div class="icon"><a href="https://vk.com/"><img src="img/vk_w.png"></a></div><div class="icon"><a href="https://www.facebook.com"><img src="img/facebook_w.png"></a></div></div>';
+*/
+/*
+	elem_footer.innerHTML = '<object type="text/html" data="v0000.html"></object>';
+*/
+}
