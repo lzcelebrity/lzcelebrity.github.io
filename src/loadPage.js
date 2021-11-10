5a77bdfc78d526a87056b30cdf2708aaa4786afe
lzcelebrity <93430938+lzcelebrity@users.noreply.github.com>
2021-11-10 13:51:57 +0200

    Add files via upload

--- src/loadPage.js
+++ src/loadPage.js
@@ -19,17 +19,17 @@ function readXMLHttp(URL, OUT){
 
 function loadPage(){
 
-	readXMLHttp("http://lzcelebrity.github.io/header.html", "header");
-	readXMLHttp("http://lzcelebrity.github.io/footer.html", "footer");
+	//readXMLHttp("http://lzcelebrity.github.io/header.html", "header");
+	//readXMLHttp("http://lzcelebrity.github.io/footer.html", "footer");
 	
 	// HEADER
 	let elem_header = document.getElementById("header");
-
-	//elem_header.innerHTML = 'Ok';
+	elem_header.innerHTML = '<div class="logo" onclick="location.href="index.html";"></div><div class="navbar"><a href="index.html"></a><div class="dropdown"><button class="dropbtn" onclick="myFunction()"><i class="fa fa-caret-down"></i></button><div class="dropdown-content" id="myDropdown"><a href="index.html">магазин</a><a href="payment.html">информация</a><a href="contact.html">контакты</a></div></div> </div>';
 
 	// FOOTER
 	let elem_footer = document.getElementById("footer");
-	//elem_footer.innerHTML = 'Ok';
+	elem_footer.innerHTML = '<div class="information"><br>LZ Celebrity<br><br><p> Designer L. Zaitseva </p><p> Ukrainian fashion brand </p><p> <a href="index.html">lzcelebrity@gmail.com</a></p><p> Kyiv, Ukraine</p><div class="icon"><a href="https://www.facebook.com/lzcelebrity" target="_blank"><img src="img/facebook_w.png"></a></div><div class="icon"><a href="https://www.instagram.com/lzcelebrity" target="_blank"><img src="img/instagram_w.png"></a></div></div>';
+
 /*
 	elem_header.innerHTML = '<div class="information"><br>LZ Celebrity<br><br><p> Designer L. Zaitseva </p><p> Ukrainian fashion brand </p><p> <a href="index.html">lzcelebrity@gmail.com</a></p><p> Kyiv, Ukraine</p><div class="icon"><a href="https://www.instagram.com"><img src="img/instagram_w.png"></a></div><div class="icon"><a href="https://vk.com/"><img src="img/vk_w.png"></a></div><div class="icon"><a href="https://www.facebook.com"><img src="img/facebook_w.png"></a></div></div>';
 */
